84a0da2705d4ede7a00086fa177e9fb38fff7ed1
unknown <g.hitoshi.yokogawa@gmail.com>
2020-06-17 09:53:48 -0300

    Ponto avulso retirado.

--- Ingresso.cs
+++ Ingresso.cs
@@ -7,7 +7,7 @@ namespace Aula17Dojo
 
         public float Valor { get; set; }
         public void ImprimirValor(){
-            System.Console.WriteLine($"O valor comum é igual a R$ {Valor}.");
+            System.Console.WriteLine($"O valor comum é igual a R$ {Valor}");
         }
     }
 }
